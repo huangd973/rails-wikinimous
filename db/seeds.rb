047be85a0771cc656598e3a6758f425c9ea6938f
huang <huangdey973@gmail.com>
2017-11-21 17:16:00 +0100

    Exo2

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+require 'faker'
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all
+
+10.times do
+  art = Article.new
+  art.title = Faker::Ancient.titan
+  art.content = "Lofrefkererglehrigej;gkjdgklfdsgkfdhgkjlfdsbgjkdfsvndflkmdfk;ljgfdklsgjdskl;gre."
+  art.save
+end
